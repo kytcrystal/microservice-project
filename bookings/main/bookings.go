590ea3b17a0a6847265f38420996a786ac5f9a51
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-01-20 21:39:05 +0100

    wip: started refactoring in booking

--- bookings/main/bookings.go
+++ bookings/main/bookings.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	if err := bookings.Run(); err != nil {
+	app, err := bookings.NewApplication()
+	if err != nil {
+		log.Fatalf("Failed to initialize application: %v", err)
+	}
+	if err := app.Run(); err != nil {
 		log.Fatalf("Application terminated with error: %v", err)
 	}
 }
